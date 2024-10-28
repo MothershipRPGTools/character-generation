d916e0e3b4e2af8b374a6449cd339324ff951559
Evan Smith <me@iamevan.me>
2024-10-28 11:43:52 +0000

    Add a wounds package (#6)
    * Add a wounds package
    
    * Update lockfile
    
    * Update lockfile

--- packages/char-gen-lib/README.md
+++ packages/char-gen-lib/README.md
@@ -1,9 +1,9 @@
 # @mothership-rpg/char-gen-lib
 
 ![](https://badgers.space/npm/name/@mothership-rpg/char-gen-lib)
-![](https://badgers.space/github/release/MothershipRPGTools/character-generation-ts)
-![](https://badgers.space/github/license/MothershipRPGTools/character-generation-ts)
-![](https://badgers.space/codeberg/stars/MothershipRPGTools/character-generation-ts)
+![](https://badgers.space/github/release/MothershipRPGTools/generators-ts)
+![](https://badgers.space/github/license/MothershipRPGTools/generators-ts)
+![](https://badgers.space/codeberg/stars/MothershipRPGTools/generators-ts)
 
 
 # Character generation
---
+++ packages/wounds/src/index.ts
@@ -0,0 +1,91 @@
+import random_default from "random";
+
+export enum BluntForce {
+  "Knocked Down." = 1,
+  "Winded. [-] until you catch your breath." = 2,
+  "Sprained Angkle. [-] on Speed Checks." = 3,
+  "Concussion. [-] on mental tasks." = 4,
+  "Leg or foot broken. [-] on Speed Checks." = 5,
+  "Arm or hand broken. [-] on manual tasks." = 6,
+  "Snapped collarbone. [-] on Strength Checks." = 7,
+  "Back broken. [-] on all rolls." = 8,
+  "Skull fracture. [-] on all rolls." = 9,
+  "Spine or neck broken. Death Save." = 10,
+}
+
+export enum Bleeding {
+  "Drop Held Item" = 1,
+  "Lots of blood. Those Close gain 1 Stress." = 2,
+  "Blood in eyes. [-] until wiped clean." = 3,
+  "Laceration. Bleeding +1." = 4,
+  "Major cut. Bleeding +2." = 5,
+  "Fingers/toes severed. Bleeding +3." = 6,
+  "Hand/foot severed. Bleeding +4." = 7,
+  "Limb severed. Bleeding +5." = 8,
+  "Major artery cut. Bleeding +6." = 9,
+  "Throat slit or heart pierced. Death Save." = 10,
+}
+
+export enum Gunshot {
+  "Grazed. Knocked down." = 1,
+  "Bleeding +1." = 2,
+  "Broken rib." = 3,
+  "Fractured extremity." = 4,
+  "Internal bleeding. Bleeding +2." = 5,
+  "Lodged bullet. Surgery required." = 6,
+  "Gunshot wound to the neck." = 7,
+  "Major blood loss. Bleeding +4." = 8,
+  "Sucking chest wound. Bleeding +5." = 9,
+  "Headshot. Death Save." = 10,
+}
+
+export enum FireAndExplosives {
+  "Hair burnt. Gain 1d5 Stress." = 1,
+  "Awesome scar. +1 Minimum Stress." = 2,
+  "Singed. [-] on next action." = 3,
+  "Shrapnel/large burn." = 4,
+  "Extensive burns. -1d10 Strength." = 5,
+  "Major Burn. -2d10 Body Save." = 6,
+  "Skin grafts required. -2d10 Body Save." = 7,
+  "Limb on fire. 2d10 Damage per round." = 8,
+  "Body on fire. 3d10 Damage per round." = 9,
+  "Engulfed in fiery explosion. Death Save." = 10,
+}
+
+export enum GoreAndMassive {
+  "Vomit. [-] on next action." = 1,
+  "Awesome scar. +1 Minimum Stress." = 2,
+  "Digit mangled." = 3,
+  "Eyes gouged out." = 4,
+  "Ripped off flesh. -1d10 Strength." = 5,
+  "Paralyzed waist down." = 6,
+  "Limb severed. Bleeding +5." = 7,
+  "Impaled. Bleeding +6." = 8,
+  "Guts spooled on floor. Bleeding +7." = 9,
+  "Head explodes. No Death Save. You have died." = 10,
+}
+
+export type AllWounds =
+  | typeof BluntForce
+  | typeof Bleeding
+  | typeof Gunshot
+  | typeof FireAndExplosives
+  | typeof GoreAndMassive;
+
+export class Wound {
+  private wound?: string;
+  constructor(public woundType: AllWounds) {}
+  public rollSeverity(): string {
+    const severity = random_default.uniformInt(1, 10)();
+    this.wound = this.woundType[severity];
+    return this.wound;
+  }
+
+  toString(): string {
+    if (this.wound) {
+      return this.wound;
+    }
+
+    return "None.";
+  }
+}
